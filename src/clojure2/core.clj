1cf16461b90b4422b3ca203a02ec7c4232507016
AndreyKirgizbaev <4yma545@gmail.com>
2020-03-11 15:24:47 +0300

    add ch chan

--- src/clojure2/core.clj
+++ src/clojure2/core.clj
@@ -1,30 +1,51 @@
-
 (ns clojure2.core
   (:require [clojure.core.async
-    :as a
-    :refer [>! <! go chan go-loop]][clojure.java.io :as io]
+             :as async
+             :refer [>! <! >!! <!! put! go chan go-loop into close!]]
             [clojure.string :as str])
   (:gen-class))
 
 (def file-as-str (slurp "src/clojure2/text.txt"))
-(def lines (str/split-lines file-as-str))
+(def read (str/split file-as-str #""))
 
 (defn ch
   [c]
   (go
-    (doseq [ln lines]
-      (>! c ln)
-    )))
+    (doseq [o read]
+      (>! c o)
+      ))
+  )
+
+(defn chars-to-words
+  [words c]
+  (
+   (go-loop [counter 1]
+     (>! words (<! c))
+     (if (> counter 1000)
+       (close! words)
+       (recur (inc counter)))
+     )
+   )
+  )
+
 
 (defn -main
   [& args]
 
-  (let [c (chan)]
+  (let [c (chan)
+        n (chan)
+        words (chan 100 (comp
+                          (partition-by (complement #{" "}))
+                          (map #(apply str %))
+                          (remove #{" "})))]
 
     (go-loop []
-      (let [o (<! c)]
-        (println o))
+      (let [o (<! words)]
+        (when (not= nil o)
+          (println o)))
       (recur))
-    
-    (ch c))
-)
+
+    (ch c)
+
+    (chars-to-words words c)
+    ))
